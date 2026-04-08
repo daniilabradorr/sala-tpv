df2dca846331763dcb92a5c2be64491aab425b73
Daniel Labrador Benito <danielabrador47@gmail.com>
2026-04-08 22:23:07 +0200

    finalizando bloque 1, solo falta l alimpieza del pycahce

--- staticfiles/js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Sala TPV base cargada");
-});
-
-document.body.addEventListener("htmx:responseError", (event) => {
-  console.error("HTMX response error:", event.detail);
-});
